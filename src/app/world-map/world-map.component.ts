c5bb5ced3e4ae534192eb4ee4f693a778f97931c
Brian Hood <hoodwebdev@gmail.com>
2019-11-17 13:02:00 -0600

    Update map styling and zoom level.

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -13,9 +13,10 @@ export class WorldMapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const width = 1080;
-    const height = 1080;
-    const projection = d3.geoNaturalEarth1(); // geoConicEqualArea();
+    const width = 1975;
+    const height = 1000;
+    const projection = d3.geoMercator().translate([width / 2, height / 2]).scale(310).center([5, 30]);
+    // geoConicEqualArea(); geoNaturalEarth1();
     const svg = d3.select('#map').append('svg')
       .attr('width', width)
       .attr('height', height);
@@ -23,15 +24,17 @@ export class WorldMapComponent implements OnInit {
     const g = svg.append('g');
     g.attr('class', 'map');
 
-    d3.json('../../assets/countries-10m.json').then((topology) => {
-      const mapFeatures: FeatureCollection = t.feature(topology, topology.objects.land);
-      console.log('topology: ', topology);
+    d3.json('../../assets/countries-10m.json').then((topo) => {
+      const mapFeatures: FeatureCollection = t.feature(topo, topo.objects.land) as any as FeatureCollection;
       g.selectAll('path')
-        // .data(topology.objects.land.geometries[0].arcs)
         .data(mapFeatures.features)
         .enter()
         .append('path')
         .attr('d', path);
+      g.append('path')
+        .datum(t.mesh(topo, topo.objects.countries, (a, b) => a !== b))
+        .attr('d', path)
+        .attr('class', 'border-glow');
     });
   }
 
